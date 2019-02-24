4a936253b748280dd3e4c6ff81ba1c635abd4968
Cristiane Mayara <cris.mayara@gmail.com>
2019-02-24 19:00:59 -0300

    Creating templates using nunjucks

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 
 const app = express();
 
-const logMiddleware = (req, res, next) => {
-  console.log(
-    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
-  );
-  req.appName = "GoNode";
-  return next();
-};
-
-app.use(logMiddleware);
-
-app.get("/", (req, res) => {
-  return res.send(`Bem-vindo, ${req.query.name}`);
+nunjucks.configure("views", {
+  autoscape: true,
+  express: app,
+  watch: true
 });
 
-app.get("/nome/:name", (req, res) => {
-  return res.json({
-    message: `Welcome, ${req.params.name}`
-  });
+app.set("view engine", "njk");
+
+app.get("/", (req, res) => {
+  return res.render("list", { name: "Cris " });
 });
 
 app.listen(3000);
